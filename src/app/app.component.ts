e2f11ca045366d2e418feefe08a0d46cbf6b72e2
yashi <yashi.songarra@bhavitechnologies.com>
2020-04-16 18:16:47 +0530

    Local storege functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'to-do';
   note='';
   todoList:string[] = [];
 
+  ngOnInit()
+  {
+    this.todoList = JSON.parse(localStorage.getItem('notes')) || [];
+  }
+
   addNote()
   {
+    if(this.note)
+    {
       this.todoList.push(this.note);
+      this.updateLocal();
       this.note='';
+    }
+      
   }
   onComplete(index)
   {
     this.todoList.splice(index,1);
+    this.updateLocal();
+  }
+  updateLocal()
+  {
+    localStorage.setItem('notes',JSON.stringify(this.todoList));
   }
 }
